32bd62e2920d0f2fcef400bd3f69c17cf6384f39
Joe Chung <joechung2008@users.noreply.github.com>
2025-10-05 16:54:14 -0400

    Remove redundant format in Main.java

--- cli/src/main/java/com/github/jsonjava/Main.java
+++ cli/src/main/java/com/github/jsonjava/Main.java
@@ -15,7 +15,7 @@ public class Main {
       try (java.util.Scanner scanner = new java.util.Scanner(System.in).useDelimiter("\\A")) {
         String json = scanner.hasNext() ? scanner.next() : "";
         System.out.println(json);
-        System.out.println(String.format("%s", JSONParser.parse(json)));
+        System.out.printf("%s%n", JSONParser.parse(json));
       }
     } catch (Exception e) {
       System.err.println("Error reading file: " + e.getMessage());
